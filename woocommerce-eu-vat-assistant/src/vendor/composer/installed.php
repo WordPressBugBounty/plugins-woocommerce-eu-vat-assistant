baf8500635b21de8fa9cbd69ed153644686368fa
Sean Murphy <sean@iamseanmurphy.com>
2024-09-23 06:19:33 +0000

    2.1.8.240109

---
+++ woocommerce-eu-vat-assistant/src/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'aelia/wc-aelia-eu-vat-assistant',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '4bb108d579d694309d7517ed27516c8b56cbde0a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'aelia/wc-aelia-eu-vat-assistant' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '4bb108d579d694309d7517ed27516c8b56cbde0a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'econea/nusoap' => array(
+            'pretty_version' => 'v0.9.7',
+            'version' => '0.9.7.0',
+            'reference' => '6f9dbb82bdd64d537947707dd7873248aa152003',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../econea/nusoap',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
